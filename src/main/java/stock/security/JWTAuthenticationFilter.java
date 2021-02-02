5cb637954a043cd3bc54af54830e7c07380cd1eb
anderkovaski <anderkovaski@gmail.com>
2021-02-02 01:36:34 -0300

    Security Part V: Fix for HTTP Status login error (UNAUTHORIZED)

--- src/main/java/stock/security/JWTAuthenticationFilter.java
+++ src/main/java/stock/security/JWTAuthenticationFilter.java
@@ -1,14 +1,18 @@
 package stock.security;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,41 +20,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import stock.dto.CredentialsDTO;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-	
+
 	private AuthenticationManager authenticationManager;
 	private JWTUtil jwtUtil;
-	
-	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {		
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
 		this.authenticationManager = authenticationManager;
 		this.jwtUtil = jwtUtil;
 	}
-	
+
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) {
-		
+
 		try {
-			
+
 			CredentialsDTO credentials = new ObjectMapper().readValue(req.getInputStream(), CredentialsDTO.class);
-			
-			UsernamePasswordAuthenticationToken authToken = 
-					new UsernamePasswordAuthenticationToken(credentials.getEmail(), credentials.getPassword());
-			
+
+			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+					credentials.getEmail(), credentials.getPassword());
+
 			Authentication auth = authenticationManager.authenticate(authToken);
-			
+
 			return auth;
-			
-		} catch(IOException e) {
+
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
 	}
-	
+
 	@Override
-	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) {
-		
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+			Authentication auth) {
+
 		String username = ((UserDetailsImpl) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		res.addHeader("Authorization", "Bearer " + token);
 	}
+	
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		response.setContentType("application/json");
+		response.getWriter().append(jsonError());
+	}
 
+    private String jsonError() {
+    	
+        long date = new Date().getTime();
+        
+        return "{\"timestamp\": " + date + ", "
+            + "\"status\": 401, "
+            + "\"error\": \"Not authorized\", "
+            + "\"message\": \"Invalid email or password\", "
+            + "\"path\": \"/login\"}";
+    }	
 }
